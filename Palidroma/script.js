44c3e96d5c6e5f36c86a6c787061e9475dab083a
ketjj <jorjolianiket@gmail.com>
2022-09-23 15:22:32 +0200

    update

--- Palidroma/script.js
+++ Palidroma/script.js
@@ -4,29 +4,18 @@
 Bonus: L’inserimento avviene tramite un campo input */
 
 
-document.getElementById('btn_verify').addEventListener('click', initVerifica);
 
-function initVerifica(){
- const insertWord = document.getElementById('parola-inserita').value;
- const turnInsertWord = turnWord(insertWord);
-//  console.log (turnWord(insertWord));
+let msg = document.getElementById('output-palindroma');
 
- let outputResult;
-  if( insertWord === turnInsertWord){
-    outputResult = `La parola <em> ${insertWord} </em> è <strong>palindroma</strong>`;
-  }else{
-    outputResult = `La parola <em> ${insertWord} </em> non è <strong>palindroma</strong>`;
+document.getElementById('btn_verify').addEventListener('click', function(){
+
+  let insertedWord = document.getElementById('parola-inserita').value;
+  const reversed = insertedWord.split('').reverse().join('');
+  if(insertedWord === reversed){
+    return msg.textContent= `la parola "${insertedWord}" è palindroma`
+  } else{
+    return msg.textContent = `la parola "${insertedWord}" non lo è palindroma`
   }
+});
 
-  document.getElementById('output-palindroma').innerHTML = outputResult;
-}
 
-function turnWord(str){
-  let turnedWord ='';
-
-  for(let i = str.length -1; i >= 0; i--){
-    turnedWord += str[i];
-  }
-  
-  return turnedWord;
-}
